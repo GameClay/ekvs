cba8a371d10e7503c20fec13ce8b71d25cd130dc
Pat Wilson <zerostride@gmail.com>
2011-10-22 13:51:23 -0700

    Now using cspec to test: https://github.com/arnaudbrejeon/cspec

--- test/ekvs_test.c
+++ test/ekvs_test.c
@@ -3,43 +3,191 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include <cspec.h>
+#include <cspec_output_verbose.h>
+
+#include "../src/ekvs_internal.h"
+
+int test_malloc_count = 0;
+size_t test_allocation_total = 0;
+size_t test_free_total = 0;
+void* test_malloc(size_t size)
+{
+   char* ret = malloc(size + sizeof(size_t));
+   test_malloc_count++;
+   test_allocation_total += size;
+   *((size_t*)ret) = size;
+   return (ret + sizeof(size_t));
+}
+
+int test_realloc_count = 0;
+void* test_realloc(void* ptr, size_t size)
+{
+   test_realloc_count++;
+   if(ptr != NULL)
+   {
+      char* fptr = ((char*)ptr) - sizeof(size_t);
+      test_free_total += *((size_t*)fptr);
+      test_allocation_total += size;
+      *((size_t*)fptr) = size;
+      return realloc(fptr, size + sizeof(size_t));
+   }
+   
+   char* ret = realloc(ptr, size + sizeof(size_t));
+   test_allocation_total += size;
+   *((size_t*)ret) = size;
+   return (ret + sizeof(size_t));
+}
+
+int test_free_count = 0;
+void test_free(void* ptr)
+{
+   if(ptr != NULL)
+   {
+      char* fptr = ((char*)ptr) - sizeof(size_t);
+      test_free_count++;
+      test_free_total += *((size_t*)fptr);
+      free(fptr);
+   }
+}
+
+DESCRIBE(ekvs_open, "int ekvs_open(ekvs* store, const char* path, const ekvs_opts* opts)")
+   IT("returns EKVS_FAIL if store is NULL")
+      SHOULD_EQUAL(ekvs_open(NULL, NULL, NULL), EKVS_FAIL)
+   END_IT
+
+   IT("returns EKVS_OK if store is valid, and other parameters are NULL")
+      ekvs teststore;
+      SHOULD_EQUAL(ekvs_open(&teststore, NULL, NULL), EKVS_OK)
+      ekvs_close(teststore);
+   END_IT
+
+   IT("returns EKVS_FAIL if some custom allocators are specified, but not all")
+      ekvs teststore;
+      ekvs_opts testopts;
+      memset(&testopts, 0, sizeof(ekvs_opts));
+      testopts.user_malloc = test_malloc;
+      SHOULD_EQUAL(ekvs_open(&teststore, NULL, &testopts), EKVS_FAIL)
+   END_IT
+   
+   IT("returns EKVS_OK if all custom allocators are sepcified")
+      ekvs teststore;
+      ekvs_opts testopts;
+      memset(&testopts, 0, sizeof(ekvs_opts));
+      testopts.user_malloc = test_malloc;
+      testopts.user_realloc = test_realloc;
+      testopts.user_free = test_free;
+      SHOULD_EQUAL(ekvs_open(&teststore, NULL, &testopts), EKVS_OK)
+      ekvs_close(teststore);
+   END_IT
+
+   IT("creates a table with the specified initial table size")
+      ekvs teststore;
+      ekvs_opts testopts;
+      memset(&testopts, 0, sizeof(ekvs_opts));
+      testopts.initial_table_size = 42;
+      SHOULD_EQUAL(ekvs_open(&teststore, NULL, &testopts), EKVS_OK)
+      SHOULD_EQUAL(teststore->serialized.table_sz, 42)
+      ekvs_close(teststore);
+   END_IT
+
+   IT("creates a table with the specified grow threshold")
+      ekvs teststore;
+      ekvs_opts testopts;
+      memset(&testopts, 0, sizeof(ekvs_opts));
+      testopts.grow_threshold = 4.2f;
+      SHOULD_EQUAL(ekvs_open(&teststore, NULL, &testopts), EKVS_OK)
+      SHOULD_EQUAL(teststore->grow_threshold, 4.2f)
+      ekvs_close(teststore);
+   END_IT
+
+   IT("uses custom allocators for memory functions")
+      ekvs teststore;
+      ekvs_opts testopts;
+      memset(&testopts, 0, sizeof(ekvs_opts));
+      testopts.user_malloc = test_malloc;
+      testopts.user_realloc = test_realloc;
+      testopts.user_free = test_free;
+      SHOULD_EQUAL(ekvs_open(&teststore, NULL, &testopts), EKVS_OK)
+      ekvs_close(teststore);
+      SHOULD_NOT_EQUAL(test_malloc_count, 0)
+      /*SHOULD_NOT_EQUAL(test_realloc_count, 0)*/
+      SHOULD_NOT_EQUAL(test_free_count, 0)
+      SHOULD_EQUAL(test_allocation_total, test_free_total)
+   END_IT
+END_DESCRIBE
+
+DESCRIBE(ekvs_set_ex, "int ekvs_set_ex(ekvs store, const char* key, const void* data, size_t data_sz, char flags)")
+   IT("returns EKVS_FAIL if store is NULL")
+      SHOULD_EQUAL(ekvs_set_ex(NULL, NULL, NULL, 0, 0), EKVS_FAIL)
+   END_IT
+   
+   IT("returns EKVS_FAIL if key is NULL")
+      ekvs teststore;
+      ekvs_open(&teststore, NULL, NULL);
+      SHOULD_EQUAL(ekvs_set_ex(teststore, NULL, NULL, 0, 0), EKVS_FAIL)
+      ekvs_close(teststore);
+   END_IT
+   
+   IT("returns EKVS_OK if key is valid, and data is NULL")
+      ekvs teststore;
+      const void* get_ptr;
+      size_t get_sz = 0;
+      ekvs_open(&teststore, NULL, NULL);
+      SHOULD_EQUAL(ekvs_set_ex(teststore, "key", NULL, 0, 0), EKVS_OK)
+      SHOULD_EQUAL(ekvs_get(teststore, "key", &get_ptr, &get_sz), EKVS_OK)
+      SHOULD_EQUAL(get_sz, 0)
+      ekvs_close(teststore);
+   END_IT
+   
+   IT("should not grow the table if the ekvs_set_no_grow flag is specified")
+      ekvs teststore;
+      ekvs_opts testopts;
+      memset(&testopts, 0, sizeof(ekvs_opts));
+      testopts.initial_table_size = 1;
+      ekvs_open(&teststore, NULL, &testopts);
+      SHOULD_EQUAL(ekvs_set_ex(teststore, "key", NULL, 0, ekvs_set_no_grow), EKVS_OK)
+      SHOULD_EQUAL(ekvs_set_ex(teststore, "key2", NULL, 0, ekvs_set_no_grow), EKVS_OK)
+      SHOULD_EQUAL(ekvs_set_ex(teststore, "key3", NULL, 0, ekvs_set_no_grow), EKVS_OK)
+      SHOULD_EQUAL(teststore->serialized.table_sz, 1)
+      ekvs_close(teststore);
+   END_IT
+   
+   IT("should the table if the grow threshold is exceeded by adding a key")
+      ekvs teststore;
+      ekvs_opts testopts;
+      memset(&testopts, 0, sizeof(ekvs_opts));
+      testopts.initial_table_size = 1;
+      ekvs_open(&teststore, NULL, &testopts);
+      SHOULD_EQUAL(ekvs_set_ex(teststore, "key", NULL, 0, 0), EKVS_OK)
+      SHOULD_EQUAL(ekvs_set_ex(teststore, "key2", NULL, 0, 0), EKVS_OK)
+      SHOULD_EQUAL(ekvs_set_ex(teststore, "key3", NULL, 0, 0), EKVS_OK)
+      SHOULD_NOT_EQUAL(teststore->serialized.table_sz, 1)
+      ekvs_close(teststore);
+   END_IT
+END_DESCRIBE
+
+DESCRIBE(ekvs_get, "int ekvs_get(ekvs store, const char* key, const void** data, size_t* data_sz)")
+   IT("returns EKVS_FAIL if store is NULL")
+      const void* get_ptr;
+      size_t get_sz = 0;
+      SHOULD_EQUAL(ekvs_get(NULL, "key", &get_ptr, &get_sz), EKVS_FAIL)
+   END_IT
+   
+   IT("returns EKVS_FAIL if key is NULL")
+      ekvs teststore;
+      const void* get_ptr;
+      size_t get_sz = 0;
+      ekvs_open(&teststore, NULL, NULL);
+      SHOULD_EQUAL(ekvs_get(teststore, NULL, &get_ptr, &get_sz), EKVS_FAIL)
+      ekvs_close(teststore);
+   END_IT
+END_DESCRIBE
+
 int main()
 {
-   ekvs mystore;
-   ekvs_opts opts;
-   const char* test_str = "facepunch!";
-   const char* get_str = NULL;
-   size_t get_str_sz;
-   struct _ekvs_db_entry* entry;
-
-   memset(&opts, 0, sizeof(ekvs_opts));
-   opts.initial_table_size = 2;
-   opts.grow_threshold = 0.5f;
-
-   ekvs_open(&mystore, "test_db", &opts);
-
-   assert(ekvs_set(mystore, "face?", test_str, strlen(test_str) + 1) == EKVS_OK);
-   assert(ekvs_get(mystore, "face?", (const void**)&get_str, &get_str_sz) == EKVS_OK);
-   printf("Got back: '%s'\n", get_str);
-   assert(ekvs_snapshot(mystore, NULL) == EKVS_OK);
-   assert(ekvs_del(mystore, "face?") == EKVS_OK);
-   get_str = NULL;
-   assert(ekvs_get(mystore, "face?", (const void**)&get_str, &get_str_sz) == EKVS_NO_KEY);
-   printf("Got back: '%s'\n", get_str);
-   char buffer[L_tmpnam];
-   char buffer2[L_tmpnam];
-   for(int i = 0; i < 5000; i++)
-   {
-      tmpnam(buffer);
-      tmpnam(buffer2);
-      assert(ekvs_set_ex(mystore, buffer, buffer2, strlen(buffer2) + 1, ekvs_set_no_grow) == EKVS_OK);
-      ekvs_get(mystore, buffer, (const void**)&get_str, &get_str_sz);
-      if(get_str == NULL || strcmp(get_str, buffer2) != 0)
-      {
-         printf("key: '%s' value differs, chain error ('%s' vs '%s')\n", buffer, get_str, buffer2);
-      }
-   }
-
-   ekvs_close(mystore);
+   CSpec_Run(DESCRIPTION(ekvs_open), CSpec_NewOutputVerbose());
+   CSpec_Run(DESCRIPTION(ekvs_set_ex), CSpec_NewOutputVerbose());
+   CSpec_Run(DESCRIPTION(ekvs_get), CSpec_NewOutputVerbose());
    return 0;
 }

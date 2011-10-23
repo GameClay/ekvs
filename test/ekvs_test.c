5d855f52d507309407ddeba2b0b9b032ecf2428e
Pat Wilson <zerostride@gmail.com>
2011-10-22 21:12:06 -0700

    More tests!

--- test/ekvs_test.c
+++ test/ekvs_test.c
@@ -111,8 +111,18 @@ DESCRIBE(ekvs_open, "int ekvs_open(ekvs* store, const char* path, const ekvs_opt
       SHOULD_EQUAL(ekvs_open(&teststore, NULL, &testopts), EKVS_OK)
       ekvs_close(teststore);
       SHOULD_NOT_EQUAL(test_malloc_count, 0)
-      /*SHOULD_NOT_EQUAL(test_realloc_count, 0)*/
       SHOULD_NOT_EQUAL(test_free_count, 0)
+   END_IT
+
+   IT("does not leak memory")
+      ekvs teststore;
+      ekvs_opts testopts;
+      memset(&testopts, 0, sizeof(ekvs_opts));
+      testopts.user_malloc = test_malloc;
+      testopts.user_realloc = test_realloc;
+      testopts.user_free = test_free;
+      ekvs_open(&teststore, NULL, &testopts);
+      ekvs_close(teststore);
       SHOULD_EQUAL(test_allocation_total, test_free_total)
    END_IT
 END_DESCRIBE
@@ -180,6 +190,47 @@ DESCRIBE(ekvs_get, "int ekvs_get(ekvs store, const char* key, const void** data,
       size_t get_sz = 0;
       ekvs_open(&teststore, NULL, NULL);
       SHOULD_EQUAL(ekvs_get(teststore, NULL, &get_ptr, &get_sz), EKVS_FAIL)
+      ekvs_close(teststore);
+   END_IT
+   
+   IT("returns EKVS_NO_KEY if the key does not exist")
+      ekvs teststore;
+      const void* get_ptr;
+      size_t get_sz = 0;
+      ekvs_open(&teststore, NULL, NULL);
+      SHOULD_EQUAL(ekvs_get(teststore, "does_not_exist", &get_ptr, &get_sz), EKVS_NO_KEY)
+      ekvs_close(teststore);
+   END_IT
+   
+   IT("returns the assigned value if the key is set")
+      ekvs teststore;
+      const void* get_ptr;
+      size_t get_sz = 0;
+      ekvs_open(&teststore, NULL, NULL);
+      ekvs_set(teststore, "key", "value", 6);
+      SHOULD_EQUAL(ekvs_get(teststore, "key", &get_ptr, &get_sz), EKVS_OK)
+      SHOULD_MATCH(get_ptr, "value")
+      ekvs_close(teststore);
+   END_IT
+   
+   IT("returns the assigned value if the key is set, and chaining has occurred")
+      ekvs teststore;
+      const void* get_ptr;
+      size_t get_sz = 0;
+      ekvs_opts testopts;
+      memset(&testopts, 0, sizeof(ekvs_opts));
+      testopts.initial_table_size = 1;
+      ekvs_open(&teststore, NULL, &testopts);
+      ekvs_set_ex(teststore, "key1", "value1", 7, ekvs_set_no_grow);
+      ekvs_set_ex(teststore, "key2", "value2", 7, ekvs_set_no_grow);
+      ekvs_set_ex(teststore, "key3", "value3", 7, ekvs_set_no_grow);
+      SHOULD_EQUAL(teststore->serialized.table_sz, 1)
+      SHOULD_EQUAL(ekvs_get(teststore, "key1", &get_ptr, &get_sz), EKVS_OK)
+      SHOULD_MATCH(get_ptr, "value1")
+      SHOULD_EQUAL(ekvs_get(teststore, "key2", &get_ptr, &get_sz), EKVS_OK)
+      SHOULD_MATCH(get_ptr, "value2")
+      SHOULD_EQUAL(ekvs_get(teststore, "key3", &get_ptr, &get_sz), EKVS_OK)
+      SHOULD_MATCH(get_ptr, "value3")
       ekvs_close(teststore);
    END_IT
 END_DESCRIBE
